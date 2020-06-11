703282cdb3b45a0777101fb11a565655b9c89418
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-06-11 08:16:12 -0700

    implement parse_encrypted_packet
    this should hopefully work, but i have to implement like 4 other packets
    before the client will even send an encrypted packet for me to test this
    with xd

--- conn.c
+++ conn.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/socket.h>
 
 #include "conn.h"
 
@@ -24,7 +25,36 @@ void conn_finish(struct conn *c) {
 }
 
 int parse_encrypted_packet(struct conn *c, struct recv_packet *p) {
-	return -1;
+	/* TODO: maybe store this in the conn (pretty sure it wont change xd) */
+	int block_size = EVP_CIPHER_CTX_block_size(c->_decrypt_ctx);
+	int in_len = MAX_PACKET_LEN + block_size;
+	uint8_t in[in_len];
+	int n = recv(c->_sfd, &in, in_len, MSG_PEEK);
+	if (n < 0) {
+		perror("read");
+		return -1;
+	}
+
+	int outl = MAX_PACKET_LEN;
+	int result = EVP_CipherUpdate(c->_decrypt_ctx, p->_data, &outl, in, n);
+	if (result == 0) {
+		/* TODO: report openssl errors here and in write_encrypted_packet */
+		fprintf(stderr, "decrypt error\n");
+		return -1;
+	}
+
+	/* read packet length + id, maybe move to a function in packet.c */
+	n = read_varint(p, &(p->_packet_len));
+	if (n < 0)
+		return -1;
+	if (read_varint(p, &(p->packet_id)) < 0)
+		return -1;
+	p->_index = 0;
+
+	/* pop the parsed packet off the read buffer */
+	size_t read_len = p->_packet_len + n + block_size;
+	read(c->_sfd, &in, read_len);
+	return p->_packet_len;
 }
 
 int write_encrypted_packet(struct conn *c, const struct send_packet *p) {
