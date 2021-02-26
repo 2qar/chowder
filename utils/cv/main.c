24f8d55466188e2e9f7956bf6cb167ae6c077e0a
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2021-02-25 20:25:24 -0800

    cv: print the block at world coordinates when given

--- utils/cv/main.c
+++ utils/cv/main.c
@@ -5,6 +5,9 @@
  *
  * Calling it with an extra number, eg. "cv r.0.0.mca 0,0,5", will print the
  * section at that index, if the number you gave is a valid index.
+ *
+ * Prefixing the "x,y,z" coordinates with a 'w' will print the block in
+ * the world at those global coordinates.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,14 +27,22 @@ struct pos {
 	int z;
 	int section;
 };
+
+struct world_pos {
+	int x;
+	int y;
+	int z;
+};
 int is_region_file(const char *filename);
 struct pos *parse_chunk_pos(const char *in);
+struct world_pos *parse_world_pos(const char *in);
 struct chunk *chunk_at(const char *filename, int x, int z);
 void print_section(struct section *, const struct pos *);
 void print_sections(struct chunk *, struct pos *);
+void print_block_at(struct chunk *, struct world_pos *);
 
 int main(int argc, char **argv) {
-	if (argc <= 1) {
+	if (argc < 3) {
 		printf("usage: cv FILE [COORDS...]\n");
 		exit(EXIT_FAILURE);
 	}
@@ -41,10 +52,20 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
-	struct pos *p = parse_chunk_pos(argv[2]);
-	if (p == NULL) {
-		fprintf(stderr, "cv: expected \"x,z\", got \"%s\"\n", argv[2]);
-		exit(EXIT_FAILURE);
+	int world_coords = argv[2][0] == 'w';
+	void *p;
+	if (world_coords) {
+		p = parse_world_pos(argv[2]);
+		if (p == NULL) {
+			fprintf(stderr, "cv: expected \"x,y,z\", got \"%s\"\n", argv[2]);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		p = parse_chunk_pos(argv[2]);
+		if (p == NULL) {
+			fprintf(stderr, "cv: expected \"x,z\", got \"%s\"\n", argv[2]);
+			exit(EXIT_FAILURE);
+		}
 	}
 	
 	int failed = create_block_table(BLOCKS_JSON_PATH);
@@ -52,19 +73,34 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "cv: error creating block table\n");
 		exit(EXIT_FAILURE);
 	}
-	struct chunk *c = chunk_at(argv[1], p->x, p->z);
-
-	if (p->section != -1) {
-		if (p->section < c->sections_len) {
-			print_section(c->sections[p->section], p);
-		} else {
-			fprintf(stderr, "cv: %d > chunk sections length\n", p->section);
-			exit(EXIT_FAILURE);
-		}
+	struct chunk *c;
+	if (world_coords) {
+		struct world_pos *w = p;
+		int x = w->x / 16;
+		int z = w->z / 16;
+		c = chunk_at(argv[1], x, z);
 	} else {
-		print_sections(c, p);
+		struct pos *pos = p;
+		c = chunk_at(argv[1], pos->x, pos->z);
 	}
 
+	if (world_coords) {
+		print_block_at(c, (struct world_pos *) p);
+	} else {
+		struct pos *pos = p;
+		if (pos->section != -1) {
+			if (pos->section < c->sections_len) {
+				print_section(c->sections[pos->section], pos);
+			} else {
+				fprintf(stderr, "cv: %d > chunk sections length\n", pos->section);
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			print_sections(c, pos);
+		}
+	}
+
+	free(p);
 	hdestroy();
 	exit(EXIT_SUCCESS);
 }
@@ -81,10 +117,19 @@ struct pos *parse_chunk_pos(const char *in) {
 
 	int parsed = sscanf(in, "%d,%d,%d", &(p->x), &(p->z), &(p->section));
 	if (parsed < 2) {
-		return NULL;
-	} else {
-		return p;
+		p = NULL;
 	}
+	return p;
+}
+
+struct world_pos *parse_world_pos(const char *in) {
+	struct world_pos *p = malloc(sizeof(struct world_pos));
+
+	int parsed = sscanf(in, "w%d,%d,%d", &(p->x), &(p->y), (&p->z));
+	if (parsed != 3) {
+		p = NULL;
+	}
+	return p;
 }
 
 struct chunk *chunk_at(const char *filename, int x, int z) {
@@ -131,4 +176,17 @@ void print_sections(struct chunk *c, struct pos *p) {
 		p->section = i;
 		print_section(c->sections[i], p);
 	}
+}
+
+void print_block_at(struct chunk *c, struct world_pos *w) {
+	int i = (w->y / 16) + 1;
+	if (i < 0 || i >= c->sections_len) {
+		fprintf(stderr, "cv: invalid y coordinate %d\n", w->y);
+		exit(EXIT_FAILURE);
+	}
+	struct section *s = c->sections[i];
+	i = read_blockstate_at(s, w->x, w->y, w->z);
+	printf("section coords: (%d,%d,%d)\n", w->x / 16, s->y, w->z / 16);
+	printf("global coords: (%d,%d,%d) = %s\n", w->x, w->y, w->z, block_names[s->palette[i]]);
+	printf("in-chunk coords: (%d,%d,%d)\n", w->x % 16, w->y % 16, w->z % 16);
 }
